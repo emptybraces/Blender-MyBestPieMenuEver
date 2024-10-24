724a27c820d2ad309ad393d8a19770655eb42cfd
{} <motose6@gmail.com>
2024-10-25 01:02:41 +0900

    up

--- my_best_pie_menu_ever/_MenuTexturePaint.py
+++ my_best_pie_menu_ever/_MenuTexturePaint.py
@@ -9,18 +9,19 @@ key_keydown_ctrl = None
 # --------------------------------------------------------------------------------
 # テクスチャペイントメニュー
 # --------------------------------------------------------------------------------
+
+
 def MenuPrimary(pie, context):
     box_root = pie.split().box()
-    box_root.label(text = 'TexturePaint Primary')
+    box_root.label(text='TexturePaint Primary')
 
     # Utility
-    box_root.operator("image.save_all_modified", text="Save All Image") 
-
+    box_root.operator("image.save_all_modified", text="Save All Image")
 
     # Brush, Stroke, Blend...
-    row = box_root.row(align=True) 
+    row = box_root.row(align=True)
     box = row.box()
-    box.label(text = "Brush")
+    box.label(text="Brush")
     r2 = box.row(align=True)
     c2 = r2.column(align=True)
     cnt = 0
@@ -30,12 +31,13 @@ def MenuPrimary(pie, context):
         if i.use_paint_image and i.name.lower() not in brush_exclude_list:
             is_use = context.tool_settings.image_paint.brush.name == i.name
             _Util.MPM_OT_SetPointer.operator(c2, i.name, context.tool_settings.image_paint, "brush", i, depress=is_use)
-            cnt += 1;
-            if cnt % limit_rows == 0: c2 = r2.column(align=True)
+            cnt += 1
+            if cnt % limit_rows == 0:
+                c2 = r2.column(align=True)
 
-    #Color picker
+    # Color picker
     box = row.box()
-    box.label(text = "Color")
+    box.label(text="Color")
     c = box.column(align=True)
     r2 = c.row(align=True)
     r3 = r2.row(align=True)
@@ -51,29 +53,33 @@ def MenuPrimary(pie, context):
     # Strokes
     cnt = 0
     box = row.box()
-    box.label(text = "Stroke")
+    box.label(text="Stroke")
     r2 = box.row(align=True)
     c2 = r2.column(align=True)
     for i in _Util.enum_values(context.tool_settings.image_paint.brush, 'stroke_method'):
         is_use = context.tool_settings.image_paint.brush.stroke_method == i
-        _Util.MPM_OT_SetterBase.operator(c2, _Util.MPM_OT_SetString.bl_idname, i, context.tool_settings.image_paint.brush, "stroke_method", i, depress=is_use)
-        cnt += 1;
-        if cnt % limit_rows == 0: c2 = r2.column(align=True)
+        _Util.MPM_OT_SetterBase.operator(c2, _Util.MPM_OT_SetString.bl_idname, i,
+                                         context.tool_settings.image_paint.brush, "stroke_method", i, depress=is_use)
+        cnt += 1
+        if cnt % limit_rows == 0:
+            c2 = r2.column(align=True)
     # Blends
     blend_include_list = [item.strip() for item in _AddonPreferences.Accessor.get_image_paint_blend_include().lower().split(',')]
     cnt = 0
     box = row.box()
-    box.label(text = "Blend")
+    box.label(text="Blend")
     r2 = box.row(align=True)
     c2 = r2.column(align=True)
     for i in _Util.enum_values(context.tool_settings.image_paint.brush, 'blend'):
         if i.lower() in blend_include_list:
             is_use = context.tool_settings.image_paint.brush.blend == i
-            # c2.operator(OT_TexPaint_Blend.bl_idname, text=i, depress=is_use).methodName = i  
-            _Util.MPM_OT_SetterBase.operator(c2, _Util.MPM_OT_SetString.bl_idname, i, context.tool_settings.image_paint.brush, "blend", i, depress=is_use)
-            # context.tool_settings.image_paint.brush.blend = self.methodName  
-            cnt += 1;
-            if cnt % limit_rows == 0: c2 = r2.column(align=True)
+            # c2.operator(OT_TexPaint_Blend.bl_idname, text=i, depress=is_use).methodName = i
+            _Util.MPM_OT_SetterBase.operator(c2, _Util.MPM_OT_SetString.bl_idname, i,
+                                             context.tool_settings.image_paint.brush, "blend", i, depress=is_use)
+            # context.tool_settings.image_paint.brush.blend = self.methodName
+            cnt += 1
+            if cnt % limit_rows == 0:
+                c2 = r2.column(align=True)
 
     # brush proeprty
     c = box_root.column(align=True)
@@ -96,35 +102,39 @@ def MenuPrimary(pie, context):
     _Util.MPM_OT_SetSingle.operator(r, "200%", brush, "strength", brush.strength * 2)
     _Util.MPM_OT_SetSingle.operator(r, "0.1", brush, "strength", 0.1)
     _Util.MPM_OT_SetSingle.operator(r, "1.0", brush, "strength", 1.0)
-    
+
     # Etc
     c = box_root.column(align=True)
-    row = c.row();
+    row = c.row()
     _Util.MPM_OT_SetSingle.operator(r, "50%", brush, "strength", brush.strength / 2)
+    _Util.layout_prop(row, unified_paint_settings, "use_unified_size")
     _Util.layout_prop(row, brush, "use_accumulate")
 
-    DrawBrushAngle(context, row);
-    DrawMirrorOption(context, row);
+    DrawBrushAngle(context, row)
+    DrawMirrorOption(context, row)
 
     DrawBehaviourOfControlKey(c)
 
+
 def DrawBehaviourOfControlKey(layout):
     row = layout.row(align=True)
-    row.label(text = "Holding Ctrl Key is")
+    row.label(text="Holding Ctrl Key is")
     ctrl_behaviour = _AddonPreferences.Accessor.get_image_paint_ctrl_behaviour()
-    if ctrl_behaviour: # Erase Alpha mode
+    if ctrl_behaviour:  # Erase Alpha mode
         _Util.layout_operator(row, MPM_OT_TexPaint_ToggleCtrlBehaviour.bl_idname, "SubColor", depress=False)
         _Util.layout_operator(row, _Util.MPM_OT_Empty.bl_idname, "Erase Alpha", False, depress=True)
     else:
         _Util.layout_operator(row, _Util.MPM_OT_Empty.bl_idname, "SubColor", False, depress=True)
         _Util.layout_operator(row, MPM_OT_TexPaint_ToggleCtrlBehaviour.bl_idname, "Erase Alpha", depress=False)
 
+
 def DrawBrushAngle(context, layout):
     row = layout.row(align=True)
-    row.label(text = "Angle")
+    row.label(text="Angle")
     from math import pi
     _Util.MPM_OT_SetterBase.operator(row, _Util.MPM_OT_SetSingle.bl_idname, "0", context.tool_settings.image_paint.brush.texture_slot, "angle", 0)
     _Util.MPM_OT_SetterBase.operator(row, _Util.MPM_OT_SetSingle.bl_idname, "180", context.tool_settings.image_paint.brush.texture_slot, "angle", pi)
+
 
 def DrawMirrorOption(context, layout):
     mrow, msub = _Util.layout_for_mirror(layout)
@@ -135,39 +145,53 @@ def DrawMirrorOption(context, layout):
     _Util.MPM_OT_SetterBase.operator(msub, _Util.MPM_OT_SetBoolToggle.bl_idname, "Y", context.object, "use_mesh_mirror_y")
     _Util.MPM_OT_SetterBase.operator(msub, _Util.MPM_OT_SetBoolToggle.bl_idname, "Z", context.object, "use_mesh_mirror_z")
 # --------------------------------------------------------------------------------
+
+
 def MenuSecondary(pie, context):
     box = pie.split().box()
-    box.label(text = 'Texture Paint Seconday')
+    box.label(text='Texture Paint Seconday')
 
 # --------------------------------------------------------------------------------
+
+
 class MPM_OT_TexPaint_SwapColor(bpy.types.Operator):
     bl_idname = "op.mpm_texpaint_swap_color"
     bl_label = ""
     bl_options = {'REGISTER', 'UNDO'}
+
     def execute(self, context):
         brush = context.tool_settings.image_paint.brush
         color = brush.color.copy()
         brush.color = brush.secondary_color.copy()
         brush.secondary_color = color
         return {'FINISHED'}
+
+
 class MPM_OT_TexPaint_SetBlack(bpy.types.Operator):
     bl_idname = "op.mpm_texpaint_set_black"
     bl_label = "Set Black"
     bl_options = {'REGISTER', 'UNDO'}
+
     def execute(self, context):
-        context.tool_settings.image_paint.brush.color = mathutils.Color((0.0,0.0,0.0))
+        context.tool_settings.image_paint.brush.color = mathutils.Color((0.0, 0.0, 0.0))
         return {'FINISHED'}
+
+
 class MPM_OT_TexPaint_SetWhite(bpy.types.Operator):
     bl_idname = "op.mpm_texpaint_set_white"
     bl_label = "Set White"
     bl_options = {'REGISTER', 'UNDO'}
+
     def execute(self, context):
-        context.tool_settings.image_paint.brush.color = mathutils.Color((1.0,1.0,1.0))
+        context.tool_settings.image_paint.brush.color = mathutils.Color((1.0, 1.0, 1.0))
         return {'FINISHED'}
+
+
 class MPM_OT_TexPaint_ToggleCtrlBehaviour(bpy.types.Operator):
     bl_idname = "op.mpm_texpaint_toggle_ctrl_behaviour"
     bl_label = "Toggle Ctrl Behaviour"
     bl_options = {'REGISTER', 'UNDO'}
+
     def execute(self, context):
         new_value = not _AddonPreferences.Accessor.get_image_paint_ctrl_behaviour()
         _AddonPreferences.Accessor.set_image_paint_ctrl_behaviour(new_value)
@@ -178,14 +202,20 @@ class MPM_OT_TexPaint_ToggleCtrlBehaviour(bpy.types.Operator):
         key_keydown_ctrl.active = new_value
         key_ctrl_lmb_invert.active = not new_value
         return {'FINISHED'}
+
+
 g_lastBlend = ""
 g_lastBrushName = ""
+
+
 class MPM_OT_TexPaint_SwitchCtrlBehaviour(bpy.types.Operator):
     bl_idname = "op.mpm_texpaint_switch_ctrl_behaviour"
     bl_label = "Switch Ctrl Behaviour"
+
     @classmethod
     def poll(cls, context):
         return context.tool_settings.image_paint.brush.stroke_method != "CURVE"
+
     def modal(self, context, event):
         context.area.tag_redraw()
         if event.ctrl:
@@ -196,17 +226,22 @@ class MPM_OT_TexPaint_SwitchCtrlBehaviour(bpy.types.Operator):
             g_lastBlend = ""
         context.window_manager.event_timer_remove(self._timer)
         return {'FINISHED'}
+
     def execute(self, context):
         global g_lastBlend
         g_lastBlend = context.tool_settings.image_paint.brush.blend
-        if g_lastBlend == 'ERASE_ALPHA': g_lastBlend = 'MIX'
+        if g_lastBlend == 'ERASE_ALPHA':
+            g_lastBlend = 'MIX'
         context.tool_settings.image_paint.brush.blend = 'ERASE_ALPHA'
         self._timer = context.window_manager.event_timer_add(0.1, window=context.window)
         context.window_manager.modal_handler_add(self)
         return {'RUNNING_MODAL'}
+
+
 class MPM_OT_TexPaint_ChangeSoften(bpy.types.Operator):
     bl_idname = "op.mpm_texpaint_change_soften"
     bl_label = "Change Soften"
+
     def modal(self, context, event):
         context.area.tag_redraw()
         if event.shift:
@@ -218,6 +253,7 @@ class MPM_OT_TexPaint_ChangeSoften(bpy.types.Operator):
             g_lastBrushName = ""
         context.window_manager.event_timer_remove(self._timer)
         return {'FINISHED'}
+
     def execute(self, context):
         global g_lastBrushName
         g_lastBrushName = context.tool_settings.image_paint.brush.name
@@ -231,17 +267,21 @@ class MPM_OT_TexPaint_ChangeSoften(bpy.types.Operator):
             _Util.show_msgbox("Set a valid brush name in the preferences.", icon='ERROR')
             return {'CANCELLED'}
 # --------------------------------------------------------------------------------
+
+
 class MPM_PT_BrushColorPalettePanel(bpy.types.Panel):
-    bl_label=""
+    bl_label = ""
     bl_space_type = "VIEW_3D"
     bl_region_type = "HEADER"
+
     def draw(self, context):
         layout = self.layout
         settings = context.tool_settings.image_paint
         layout.template_ID(settings, "palette", new="palette.new")
         if settings.palette:
-           layout.template_palette(settings, "palette", color=True)
+            layout.template_palette(settings, "palette", color=True)
 # --------------------------------------------------------------------------------
+
 
 classes = (
     MPM_OT_TexPaint_SwapColor,
@@ -255,6 +295,7 @@ classes = (
 
 addon_keymaps = []
 
+
 def register():
     _Util.register_classes(classes)
     # Keymaps
@@ -263,17 +304,17 @@ def register():
     global key_keydown_ctrl
     wm = bpy.context.window_manager
     km = wm.keyconfigs.addon.keymaps.new(name='Image Paint', space_type='EMPTY')
-    key_keydown_ctrl = km.keymap_items.new(MPM_OT_TexPaint_SwitchCtrlBehaviour.bl_idname, 'LEFT_CTRL','PRESS')
+    key_keydown_ctrl = km.keymap_items.new(MPM_OT_TexPaint_SwitchCtrlBehaviour.bl_idname, 'LEFT_CTRL', 'PRESS')
     key_keydown_ctrl.active = _AddonPreferences.Accessor.get_image_paint_ctrl_behaviour()
     addon_keymaps.append((km, key_keydown_ctrl))
-    
+
     km = wm.keyconfigs.addon.keymaps.new(name='Image Paint', space_type='EMPTY')
-    key_ctrl_lmb_erasealpha = km.keymap_items.new("paint.image_paint", 'LEFTMOUSE','PRESS', ctrl=True)
+    key_ctrl_lmb_erasealpha = km.keymap_items.new("paint.image_paint", 'LEFTMOUSE', 'PRESS', ctrl=True)
     key_ctrl_lmb_erasealpha.active = _AddonPreferences.Accessor.get_image_paint_ctrl_behaviour()
     addon_keymaps.append((km, key_ctrl_lmb_erasealpha))
-    
+
     km = wm.keyconfigs.addon.keymaps.new(name='Image Paint', space_type='EMPTY')
-    key_ctrl_lmb_invert = km.keymap_items.new("paint.image_paint", 'LEFTMOUSE','PRESS', ctrl=True)
+    key_ctrl_lmb_invert = km.keymap_items.new("paint.image_paint", 'LEFTMOUSE', 'PRESS', ctrl=True)
     key_ctrl_lmb_invert.active = not _AddonPreferences.Accessor.get_image_paint_ctrl_behaviour()
     key_ctrl_lmb_invert.properties.mode = 'INVERT'
     addon_keymaps.append((km, key_ctrl_lmb_invert))
@@ -286,6 +327,7 @@ def register():
     kmi = km.keymap_items.new("paint.image_paint", 'LEFTMOUSE', 'PRESS', shift=True)
     kmi.properties.mode = 'SMOOTH'
     addon_keymaps.append((km, kmi))
+
 
 def unregister():
     _Util.unregister_classes(classes)
